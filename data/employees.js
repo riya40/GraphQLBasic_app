9b4c7a59fdfb34f2423e78c9946a7b7c7b75f4b0
riya40 <priyanunna20@gmail.com>
2023-05-19 17:31:54 +0530

    Added:employee data file

---
+++ data/employees.js
@@ -0,0 +1,15 @@
+const employees = [
+    {
+        id:1,
+        emp_name:'John Varghese',
+        emp_designation:'IT Manager',
+        emp_address:'Pune'
+    },
+    {
+        id:2,
+        emp_name:'Adam smith',
+        emp_designation:'Developer',
+        emp_address:'Germany'
+    }]
+
+module.exports = employees
